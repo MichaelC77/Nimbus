3f84979ebad9f2449448cf04bfd5f1add00595ff
Brad <brad.parrell00@ed.cna.nl.ca>
2018-11-22 19:04:25 -0330

    game.js: implemented image arrays (upper and lower bound) randomizer, applied images to clicked images.
    nimbus.css: centered game platform. set image ratios for card-background and applied card front images.
    
    game.html: changed card alt attributes to numbers (0-19)

--- game.js
+++ game.js
@@ -1,18 +1,51 @@
 $(document).ready(function () {
 
-    function shuffle(a) {
-        for (let i = a.length - 1; i > 0; i--) {
+    /**
+     * Randomizer array shuffle
+     * Takes in an array and shuffles the index positions using
+     * Math.random to prevent predictable outputs and
+     * returns a re-ordered array.
+     * @param array
+     * @returns {array}
+     */
+    function shuffle(array) {
+        for (let i = array.length - 1; i > 0; i--) {
             const j = Math.floor(Math.random() * (i + 1));
-            [a[i], a[j]] = [a[j], a[i]];
+            [array[i], array[j]] = [array[j], array[i]];
         }
-        return a;
+        return array;
     }
 
-    var myArray = ['1','2','3','4','5','6','7','8','9'];
-    console.log(shuffle(myArray));
+    /**
+     * Global image arrays
+     * imageArrayOne is a lower bound image array for the first 10 cards
+     * imageArrayTwo is a higher bound image array for the last 10 cards
+     */
+    var imageArrayOne = ['train.png', 'plane.png', 'flower.png', 'cow.png',
+        'car.png', 'boat.png', 'snake.jpg', 'rocket.jpg', 'dreamySnakes.jpg',
+        'chicken.jpg'];
+    var imageArrayTwo = imageArrayOne.slice();
+    shuffle(imageArrayOne);
+    shuffle(imageArrayTwo);
+
+    /**
+     * Clickevent handler
+     */
+    // noinspection JSDeprecatedSymbols
+    $("table img").click(function () {
+        if ((parseInt($(this).attr('alt'))) > 9){
+            $(this).attr('src',
+                imageArrayTwo[(
+                    parseInt($(this).attr(
+                        'alt')) -10)]);
+        } else {
+            $(this).attr('src', imageArrayOne[parseInt(
+                $(this).attr('alt')
+            )]);
+        }
+    });
 
     /** TODO: Use an array of img src
-     * randomize shuffle array
      * index 0 is card 1 etc..
      * TODO: create img image on click function
      * fadeOut/explode
@@ -21,3 +54,12 @@ $(document).ready(function () {
      * fade in img
      */
 });
+
+// ===========================DEBUG CODE=============================
+/*
+    $("table img").attr("id", function (index) {
+        var id = "heading" + (index + 1);
+        $(this).attr('src', imageArrayOne[index]);
+        imageArrayOne.push($(this).attr("src"));
+    });
+    */
